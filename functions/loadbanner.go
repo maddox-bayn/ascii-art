8bd77f047536aee9629e0448ef5251fba5742dde
emoduh <eoduh22@gmail.com>
2026-04-11 17:28:32 +0100

    to main

---
+++ functions/loadbanner.go
@@ -0,0 +1,40 @@
+package functions
+
+import (
+	"bufio"
+	"os"
+)
+
+func LoadBanner(filename string) ([][]string, error) {
+
+	bannerf, err := os.Open(filename)
+
+	if err != nil {
+		return nil, err
+	}
+	defer bannerf.Close()
+
+	scanner := bufio.NewScanner(bannerf)
+
+	var fileLines []string
+	asciiTable := make([][]string, 127)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fileLines = append(fileLines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	for i := 32; i < 127; i++ {
+		startIndex := (i-32)*9 + 1
+		endIndex := startIndex + 8
+
+		if len(fileLines) <= 126 {
+			asciiTable[i] = fileLines[startIndex:endIndex]
+		}
+	}
+	return asciiTable, nil
+}
--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	input := os.Args[1]
+
+	fmt.Println(input)
+
+	inputArg := strings.Split(input, "\n")
+	fmt.Println(inputArg)
+
+	for i := 0; i < len(inputArg); i++ {
+		for j := 0; j <= 7; j++ {
+			for k := 0; k < len(inputArg[i]); k++ {
+				fmt.Print(string(inputArg[i][k]))
+			}
+		}
+	}
+
+}
